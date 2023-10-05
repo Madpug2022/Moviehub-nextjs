b3e3cd3d9c426d8b034d7e8f2f186e27af6b9924
Matias Alaimo <matias.chiappa@hotmail.com>
2023-10-05 15:43:16 +0200

    Preparing deployment

--- app/api/brand/route.ts
+++ app/api/brand/route.ts
@@ -12,3 +12,18 @@ export async function GET(req: Request, res: Response) {
     }
 
 }
+export const POST = async (req: Request) => {
+    try {
+        const { img, href } = await req.json();
+
+        const newGenre = await prisma.brand.create({
+            data: {
+                img: img,
+                href: href
+            }
+        })
+        return NextResponse.json(newGenre, { status: 200 });
+    } catch (err) {
+        return NextResponse.json(err, { status: 500 });
+    }
+}
